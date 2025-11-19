bb2aac4975b4bac36598e09ada4fe2f35ae1d95c
Porokha <Porokha@gstore.ge>
2025-11-19 17:00:42 +0400

    Fix SLA module with ScheduleModule and SlaScheduler

--- backend/src/sla/sla.module.ts
+++ backend/src/sla/sla.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
 import { SlaService } from './sla.service';
 import { SlaController } from './sla.controller';
+import { SlaScheduler } from './sla.scheduler';
 import { ServiceCase } from '../cases/entities/service-case.entity';
 import { SmsModule } from '../sms/sms.module';
 
@@ -9,10 +11,10 @@ import { SmsModule } from '../sms/sms.module';
   imports: [
     TypeOrmModule.forFeature([ServiceCase]),
     SmsModule,
+    ScheduleModule.forRoot(),
   ],
   controllers: [SlaController],
-  providers: [SlaService],
+  providers: [SlaService, SlaScheduler],
   exports: [SlaService],
 })
 export class SlaModule {}
-
